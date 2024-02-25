cda3d0d85a15006b78db39efa2d744ef3b7ea705
Mr_Zet <149635180+Zetreit@users.noreply.github.com>
2024-02-26 00:01:19 +0300

    Update Lesson_9_Sets.py

--- Lesson_9_Sets.py
+++ Lesson_9_Sets.py
@@ -1,69 +1,39 @@
-#Задание №1
+# Задание №1
+def count_unique_numbers(N, numbers):
+    unique_numbers = set(numbers)
+    return len(unique_numbers)
 
-# Ввод числа N
-N = int(input())
+# Задание №2
+def count_common_numbers(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    common_numbers = set1.intersection(set2)
+    return len(common_numbers)
 
-# Создание пустого списка для чисел
-numbers = []
+# Задание №3
+def check_numbers(sequence):
+    seen = set()
+    for num in sequence:
+        if num in seen:
+            print("YES")
+        else:
+            print("NO")
+            seen.add(num)
 
-# Ввод N чисел и добавление их в список
-for _ in range(N):
-    num = int(input())
-    numbers.append(num)
-
-# Переворачиваем список чисел и выводим его
-reversed_numbers = numbers[::-1]
-for num in reversed_numbers:
-    print(num)
-
-#Задание №2
-
-# Ввод числа N
-N = int(input())
-
-# Ввод N чисел через пробел и разделение их на список
-numbers = list(map(int, input().split()))
-
-# Изменение порядка элементов в списке
-reversed_numbers = numbers[::-1]
-
-# Вывод измененного списка
-for num in reversed_numbers:
-    print(num, end=' ')
-
-#Задание №3
-
-# Ввод максимальной массы лодки и количества рыбаков
-m = int(input())
-n = int(input())
-
-# Ввод веса каждого рыбака и сохранение в список
-fishermen_weights = [int(input()) for _ in range(n)]
-
-# Сортировка списка весов рыбаков по возрастанию
-fishermen_weights.sort()
-
-# Инициализация переменной для подсчета лодок
-boats_count = 0
-
-# Пока есть рыбаки в списке
-while fishermen_weights:
-    # Если в списке остался только один рыбак
-    if len(fishermen_weights) == 1:
-        # Увеличиваем количество лодок и выходим из цикла
-        boats_count += 1
-        break
-    # Если сумма весов двух самых легких рыбаков не превышает максимальную массу лодки
-    elif fishermen_weights[0] + fishermen_weights[-1] <= m:
-        # Удаляем из списка двух рыбаков и увеличиваем количество лодок
-        fishermen_weights.pop(0)
-        fishermen_weights.pop()
-        boats_count += 1
-    else:
-        # Если сумма весов двух самых легких рыбаков превышает максимальную массу лодки
-        # Удаляем из списка только самого тяжелого рыбака и увеличиваем количество лодок
-        fishermen_weights.pop()
-        boats_count += 1
-
-# Вывод количества лодок
-print(boats_count)
+# Ввод данных и вызов функций
+if __name__ == "__main__":
+    # Задание №1
+    N = int(input())
+    numbers = list(map(int, input().split()))
+    result1 = count_unique_numbers(N, numbers)
+    print(result1)
+    
+    # Задание №2
+    list1 = [int(input()) for _ in range(int(input()))]
+    list2 = [int(input()) for _ in range(int(input()))]
+    result2 = count_common_numbers(list1, list2)
+    print(result2)
+    
+    # Задание №3
+    sequence = list(map(int, input().split()))
+    check_numbers(sequence)
